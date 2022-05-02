1030d9572a285f9fb1658856358b78062105f663
Stas Medyulyanov <medyulyan93@gmail.com>
2022-05-02 23:09:03 +0300

    все блоки + адаптив

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,9 +1,21 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
- //import { isMobile } from "./functions.js";
- // import { formsModules } from "./forms/forms.js";
+import { isMobile } from "./functions.js";
+// import { formsModules } from "./forms/forms.js";
 
 //  if (isMobile.any()) {
 //    document.body.classList.add("_touch");
 //  } else {
 //    document.body.classList.add("_pc");
 //  }
+
+// if (isMobile.any()) {
+//   let navLinkSublist = document.querySelectorAll(".nav__link_sublist");
+//   let navSublist = document.querySelectorAll(".nav__sublist");
+//   if (navLinkSublist.length > 0 && navSublist.length > 0) {
+//     navLinkSublist.addEventListener("click", function (e) {});
+//     navSublist.classList.toggle("_active");
+//   }
+// }
+
+
+
